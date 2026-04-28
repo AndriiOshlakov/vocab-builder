63ee559ac54abd98498c40c358bd46a2d5cedd95
Andrii <andreyoshlakov18051986@gmail.com>
2026-04-28 20:32:05 +0300

    added dinamic to dictionary

--- app/(private)/dictionary/page.tsx
+++ app/(private)/dictionary/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import { useState } from 'react';
 import css from './Dictionary.module.css';
 import { useQuery } from '@tanstack/react-query';
